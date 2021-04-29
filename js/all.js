c2a348663620aeaad0b1e03ccbb7a4f5fc2f2b6d
rousong <recall131466@gmail.com>
2021-04-29 17:20:31 +0800

    製作土地查詢頁面

--- js/all.js
+++ js/all.js
@@ -7,18 +7,24 @@ $(document).ready(function () {
     $('.menu__list').toggleClass('is-active');
   });
 
-  // 切換搜尋list內容
+  // 切換list內容
   let index = 0;
-  $('.search__item').eq(index).addClass('is-active');
+  // $('.search__item').eq(index).addClass('is-active');
   $('.search__content').eq(index).show();
+  // $('.querylist__content').eq(index).show();
 
   $('.search__item').on('click', function () {
     index = $(this).index();
 
     $('.search__item').eq(index).addClass('is-active').siblings().removeClass('is-active');
-    console.log(this);
+
+    // 首頁查詢
     $('.search__content').hide();
     $('.search__content').eq(index).fadeIn();
+
+    // 土地查詢
+    $('.querylist__content').hide();
+    $('.querylist__content').eq(index).fadeIn();
 
   });
 
@@ -27,7 +33,6 @@ $(document).ready(function () {
     let id = $(this).data('id');
     console.log(id);
     $(id).addClass('is-active');
-
   })
 
   // 手機版-PC-點擊查詢顯示對應內容結果
@@ -41,7 +46,7 @@ $(document).ready(function () {
   //當滑動到特定區塊時加入特定Class
   $(window).scroll(function () {
 
-    if ($(window).scrollTop() >= $('.banner__search').offset().top) {
+    if ($(window).scrollTop() >= $('.search__list').offset().top) {
       $('.header__scroll').addClass('is-active');
     } else {
       $('.header__scroll').removeClass('is-active');
@@ -54,7 +59,7 @@ $(document).ready(function () {
 
     e.preventDefault();
     $('html,body').animate({
-      scrollTop: $('.banner__search').offset().top
+      scrollTop: $('.main').offset().top
     }, 700, 'swing');
 
   });
